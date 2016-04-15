0ee895eca5995f175e85efc5afa279533152b150
Natalie Ortiz <ndnatalie@gmail.com>
2016-04-15 14:52:22 -0500

    Added the number of guesses the user made.

--- high_low.php
+++ high_low.php
@@ -2,6 +2,8 @@
 
 $number = rand(1, 100);
 
+$trys = 0;
+
 echo "Guess my number.\n";
 
 $guess = trim(fgets(STDIN)); 
@@ -9,10 +11,12 @@ $guess = trim(fgets(STDIN));
 while (0 < $guess && $guess <= 100 && $guess != $number) {
 	if ($guess < $number) {
 		echo "HIGHER!\n Try again:";
+		$trys++;
 		$guess = trim(fgets(STDIN)); 
 	} else if ($guess > $number) {
 		echo "LOWER!\n Try again:";
 		$guess = trim(fgets(STDIN));
+		$trys++;
 	} else {
 		echo "That's not a number.\n Guess a number this time:";
 		$guess = trim(fgets(STDIN));
@@ -24,5 +28,7 @@ if ($guess == 0){
 }
 
 if ($guess == $number){
+	$trys++;
 	echo "You guessed my number!\n";
+	echo "It took you {$trys} guesses!";
 }
