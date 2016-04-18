b1e2b9242ab4faf2a1a70e3943bb6b4c6fa86a4c
Natalie Ortiz <ndnatalie@gmail.com>
2016-04-18 10:32:59 -0500

    Passes user arguments.  Checks that user enters a number.

--- high_low.php
+++ high_low.php
@@ -1,14 +1,32 @@
 <?php 
 
-$number = rand(1, 100);
+$min = $argv[1];
+$max = $argv[2];
 
+//If user enters the max first, it switches value. 
+if ($min > $max){
+	$max = $argv[1];
+	$min = $argv[2];
+}
+
+//Generates random number between min and max. 
+$number = rand($min, $max);
+
+//Counts the number of trys that the user enters. 
 $trys = 0;
 
-echo "Guess my number.\n";
-
+//Asks user for a number to guess. Assigned to $guess. 
+echo "Guess a number between {$min} and {$max}.\n";
 $guess = trim(fgets(STDIN)); 
 
-while (0 < $guess && $guess <= 100 && $guess != $number) {
+//if user enters something other than a number. 
+do {
+	echo "Guess a number PLEASE.\n";
+	$guess = trim(fgets(STDIN)); 
+} while (ctype_digit ($guess) == false);
+
+//Loop checking user guesses against randomly selected number. 
+while ($min < $guess && $guess < $max && $guess != $number) {
 	if ($guess < $number) {
 		echo "HIGHER!\n Try again:";
 		$trys++;
@@ -17,16 +35,15 @@ while (0 < $guess && $guess <= 100 && $guess != $number) {
 		echo "LOWER!\n Try again:";
 		$guess = trim(fgets(STDIN));
 		$trys++;
-	} else {
-		echo "That's not a number.\n Guess a number this time:";
-		$guess = trim(fgets(STDIN));
-	}
+	} 
 }
 
+//Entering zero will exit the game. 
 if ($guess == 0){
 	echo "End game.\n";
 }
 
+//If user guesses the right number. 
 if ($guess == $number){
 	$trys++;
 	echo "You guessed my number!\n";
